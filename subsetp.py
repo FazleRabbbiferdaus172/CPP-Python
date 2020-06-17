c16e82363483dfe29544a09bd67c2a30935e6ade
FazleRabbbiferdaus172 <55714747+FazleRabbbiferdaus172@users.noreply.github.com>
2020-06-17 23:19:58 +0600

    Add files via upload

---
+++ subsetp.py
@@ -0,0 +1,14 @@
+
+def search(n, i=1, l=[]):
+    if i == n+1:
+        for j in l:
+            print(j, end=" ")
+        return
+    else:
+        l.append(i)
+        search(n, i+1, l)
+        l.pop()
+        search(n, i+1, l)
+
+
+search(5, 1)
