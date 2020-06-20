60b3447b7b384838838b20626655eaf838e006d6
FazleRabbbiferdaus172 <55714747+FazleRabbbiferdaus172@users.noreply.github.com>
2020-06-20 21:06:52 +0600

    Add files via upload

---
+++ powerof2.cpp
@@ -0,0 +1,17 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+    int x;
+    cin>>x;
+    if(x&(x-1))
+    {
+        cout<<"no";
+    }
+    else
+    {
+        cout<<"yes";
+    }
+}
---
+++ powerof2.py
@@ -0,0 +1,6 @@
+x = int(input())
+
+if x & (x-1):
+    print("no")
+else:
+    print("yes")
