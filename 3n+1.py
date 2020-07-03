ddf882ddfa4b9c2d7786f7b4bf66f90ed6637415
Fazle Rabbi Ferdaus <fazle.ferdaus1416@gmail.com>
2020-07-03 07:14:50 +0600

    Add files via upload

--- 3n+1.py
+++ 3n+1.py
@@ -1,18 +1,38 @@
-i, j = [int(x) for x in input().split()]
+import sys
+cache = {}
 
-mc, c = 1, 0
-print(i, end=" ")
-print(j, end=" ")
-if i > j:
-    i, j = j, i
-for n in range(i, j+1):
-    c = 1
-    while n > 1:
-        c += 1
-        if n % 2 == 0:
-            n /= 2
+
+def three(n):
+    if n in cache:
+        return cache[n]
+    if n == 1:
+        return 1
+    orgin = n
+    if n % 2 == 0:
+        n = n / 2
+    else:
+        n = 3*n + 1
+    count = three(n) + 1
+    cache[orgin] = count
+    return count
+
+
+def main():
+    for line in sys.stdin:
+        if not line:
+            break
+        i, j = [int(x) for x in line.split()]
+        if i > j:
+            start_point = j
+            end_point = i
         else:
-            n = n*3 + 1
-    mc = max(mc, c)
+            start_point = i
+            end_point = j
+        maxlength = 0
+        for n in range(start_point, end_point + 1):
+            maxlength = max(maxlength, three(n))
+        print(i, j, maxlength)
 
-print(mc)
+
+if __name__ == '__main__':
+    main()
